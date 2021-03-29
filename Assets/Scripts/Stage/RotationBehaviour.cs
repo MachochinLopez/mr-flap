7ef0f185c7a46023a254a754c7de6276602dbeea
Oscar López <machochinnio@hotmail.com>
2021-03-29 08:28:37 -0600

    RotationBehaviour

---
+++ Assets/Scripts/Stage/RotationBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationBehaviour : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed;   // Velocidad de rotación.
+
+    // Cada frame gira el objeto.
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+    }
+}
